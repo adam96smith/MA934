c8443a1b1098c4cbfb3164072033cb98c8a96438
adam96smith <adam.smith.1@warwick.ac.uk>
2019-10-07 20:40:44 +0100

    7/10 Update Nearly Finished

--- class-2-adam96smith/MA934Sorting.jl/src/MA934Sorting.jl
+++ class-2-adam96smith/MA934Sorting.jl/src/MA934Sorting.jl
@@ -1,10 +1,18 @@
 module MA934Sorting
 
+using BenchmarkTools
+using Statistics
+using Plots
+using JLD
+
 export Iter_Sort
 export mergepresorted
 export mergesort
+export Iter_mergesort
+export Iter_mergepresorted
 
-function Iter_Sort(a)
+function Iter_Sort(b)
+    a = copy(b)
     for i = 2:length(a)
         tmp = a[i]
         j = i-1
@@ -33,7 +41,8 @@ function mergepresorted(A::Array{Int64,1}, B::Array{Int64,1})
     end    
 end
 
-function mergesort(a)
+function mergesort(b)
+    a = copy(b)
     n = length(a)
     if n == 1
         return a
@@ -43,7 +52,45 @@ function mergesort(a)
     end
 end
 
+function Iter_mergepresorted(A::Array{Int64,1}, B::Array{Int64,1})
+    if length(A) == 0
+        return B
+    elseif length(B) == 0
+        return A
+    else
+        i = 1
+        j = 1
+        m = length(A)+length(B)
+        c = rand(0:0,m)
+        for l = 1:m
+            if i > length(A)
+                c[l] = B[j]
+                j = j+1
+            elseif j > length(B)
+                c[l] = A[i]
+                i = i+1
+            elseif A[i] < B[j]
+                c[l] = A[i]
+                i = i+1
+            else
+                c[l] = B[j]
+                j = j+1
+            end
+        end
+    end
+    return c
+end
 
+function Iter_mergesort(b)
+    a = copy(b)
+    n = length(a)
+    if n == 1
+        return a
+    else
+        m = Int(n/2)
+        return Iter_mergepresorted(Iter_mergesort(a[1:m]),Iter_mergesort(a[m+1:n]))
+    end
+end
 
 
 end
